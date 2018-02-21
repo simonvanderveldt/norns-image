a0fe95d5ae73e3e5b01ea2d9f0d87c23c664a2ba
rick venn <rv@lap.chop>
2018-02-21 22:49:19 +0000

    Add libmonome package

---
+++ package/libmonome/libmonome.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libmonome
+#
+################################################################################
+
+LIBMONOME_VERSION = v1.4.1
+LIBMONOME_SITE = $(call github,monome,libmonome,$(LIBMONOME_VERSION))
+LIBMONOME_INSTALL_STAGING = YES
+LIBMONOME_LICENSE = ISC
+LIBMONOME_LICENSE_FILES = COPYRIGHT
+LIBMONOME_CONF_OPTS = \
+	--disable-osc ;
+
+$(eval $(waf-package))
