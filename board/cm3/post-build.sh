7fca45f6779ac02ce095f56ba71334fe9a5d87bd
Simon van der Veldt <simon.vanderveldt@gmail.com>
2018-02-21 22:10:48 +0100

    Remove autoenabling of networking

--- board/cm3/post-build.sh
+++ board/cm3/post-build.sh
@@ -5,3 +5,6 @@ BOARD_DIR="$(dirname $0)"
 
 # Replace the rpi config.txt file
 cp -f ${BOARD_DIR}/config.txt $BINARIES_DIR/rpi-firmware/
+
+# Remove autoenabling of networking
+rm ${TARGET_DIR}/etc/init.d/S40network
