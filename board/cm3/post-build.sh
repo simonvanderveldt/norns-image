17a88e854d5d07f777eddd7f045334494de00715
Simon van der Veldt <simon.vanderveldt@gmail.com>
2018-03-02 22:26:41 +0100

    Enable use of networking via wlan0
    wlan0 requires configuration of SSID and paswork in /etc/wpa_supplicant.conf
    Once that's done wlan0 cann be enabled by running 'ifup wlan0'

--- board/cm3/post-build.sh
+++ board/cm3/post-build.sh
@@ -5,8 +5,3 @@ BOARD_DIR="$(dirname $0)"
 
 # Replace the rpi config.txt file
 cp -f ${BOARD_DIR}/config.txt $BINARIES_DIR/rpi-firmware/
-
-# Remove autoenabling of networking
-if [ -f ${TARGET_DIR}/etc/init.d/S40network ]; then
-  rm ${TARGET_DIR}/etc/init.d/S40network
-fi
