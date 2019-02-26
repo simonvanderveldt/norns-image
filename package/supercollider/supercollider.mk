e99f52de8fd2d03afc72eeffb6ebacc8a871178c
Simon van der Veldt <simon.vanderveldt@gmail.com>
2019-02-26 20:09:23 +0100

    package/supercollider: update to 3.9.3

--- package/supercollider/supercollider.mk
+++ package/supercollider/supercollider.mk
@@ -4,16 +4,19 @@
 #
 ################################################################################
 
-SUPERCOLLIDER_VERSION = 3.9.1
+SUPERCOLLIDER_VERSION = 3.9.3
 SUPERCOLLIDER_SOURCE = SuperCollider-$(SUPERCOLLIDER_VERSION)-Source-linux.tar.bz2
 SUPERCOLLIDER_SITE = https://github.com/supercollider/supercollider/releases/download/Version-$(SUPERCOLLIDER_VERSION)
 SUPERCOLLIDER_INSTALL_STAGING = YES
 SUPERCOLLIDER_LICENSE = GPL-3.0
 SUPERCOLLIDER_LICENSE_FILES = COPYING
+# For unknown reasons SC is not picking up on readline (or not correctly setting HAVE_READLINE during configure?)
+# So forcing that by adding it here
 SUPERCOLLIDER_CONF_OPTS = \
+  -DHAVE_READLINE=yes \
   -DCMAKE_BUILD_TYPE=Release \
   -DAUDIOAPI=jack \
-  -DSUPERNOVA=no \
+  -DSUPERNOVA=yes \
   -DSCLANG_SERVER=yes \
   -DSC_QT=no \
   -DSSE=no \
