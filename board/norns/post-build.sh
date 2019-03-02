0700f66297aed2b1f78373951b6f6547111b3518
Simon van der Veldt <simon.vanderveldt@gmail.com>
2019-03-02 20:12:57 +0100

    Add norns-specific systemd units

--- board/norns/post-build.sh
+++ board/norns/post-build.sh
@@ -5,3 +5,7 @@ BOARD_DIR="$(dirname $0)"
 
 # Replace the rpi config.txt file
 cp -f ${BOARD_DIR}/config.txt $BINARIES_DIR/
+
+# Enable norns.target
+ln -fs ../norns.target \
+		${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/norns.target
