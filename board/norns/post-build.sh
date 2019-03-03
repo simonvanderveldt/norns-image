ea9a568392a0ffa9e7e361183db50df180eb9e91
Simon van der Veldt <simon.vanderveldt@gmail.com>
2019-03-03 17:21:02 +0100

    Rename dt-blob and copy cmdline.txt in post-build script
    instead of in post-image script

--- board/norns/post-build.sh
+++ board/norns/post-build.sh
@@ -3,7 +3,13 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 
-# Replace the rpi config.txt file
+# Rename dt-blob.dtb to dt-blob.bin
+if [ -f ${BINARIES_DIR}/dt-blob.dtb ]; then
+  mv "${BINARIES_DIR}/dt-blob.dtb" "${BINARIES_DIR}/dt-blob.bin"
+fi
+
+# Use our custom cmdline.txt and config.txt
+cp -f ${BOARD_DIR}/cmdline.txt $BINARIES_DIR/
 cp -f ${BOARD_DIR}/config.txt $BINARIES_DIR/
 
 # Enable norns.target
--- board/norns/post-image.sh
+++ board/norns/post-image.sh
@@ -7,14 +7,6 @@ BOARD_NAME="$(basename ${BOARD_DIR})"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
-# Rename dt-blob.dtb to dt-blob.bin
-if [ -f ${BINARIES_DIR}/dt-blob.dtb ]; then
-  mv "${BINARIES_DIR}/dt-blob.dtb" "${BINARIES_DIR}/dt-blob.bin"
-fi
-
-# Replace the default rpi cmdline.txt
-cp -f ${BOARD_DIR}/cmdline.txt $BINARIES_DIR/
-
 rm -rf "${GENIMAGE_TMP}"
 
 genimage                           \
