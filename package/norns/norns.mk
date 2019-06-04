ca798989dae6ee6e1979642e56c115a3f857ccfb
Simon van der Veldt <simon.vanderveldt@gmail.com>
2019-06-04 23:32:25 +0200

    Install norns in system location
    So we can decouple it from the user's directory allowing for separate partitions in the future

--- package/norns/norns.mk
+++ package/norns/norns.mk
@@ -12,28 +12,4 @@ NORNS_INSTALL_STAGING = YES
 NORNS_CONF_OPTS = --boost-includes=$(STAGING_DIR)/usr/include --boost-libs $(STAGING_DIR)/usr/lib
 NORNS_DEPENDENCIES = avahi boost cairo libevdev liblo libmonome lua nanomsg supercollider
 
-# TODO temp fix to copy required files to hardcoded locations
-# These should be installed to system directories and defined in the wscript
-
-define NORNS_POST_INSTALL_TARGET_USER
-  mkdir -p $(TARGET_DIR)/root/norns
-  cp -a $(@D)/resources ${TARGET_DIR}/root/norns/resources
-  cp -a $(@D)/lua ${TARGET_DIR}/root/norns/lua
-  mkdir -p $(TARGET_DIR)/root/norns/sc
-  cp -a $(@D)/sc/core ${TARGET_DIR}/root/norns/sc/
-  cp -a $(@D)/sc/engines ${TARGET_DIR}/root/norns/sc/
-  mkdir -p $(TARGET_DIR)/root/norns/sc/ugens
-  mkdir -p ${TARGET_DIR}/root/.local/share/SuperCollider/Extensions/
-  cp -a $(@D)/sc/norns-config.sc ${TARGET_DIR}/root/.local/share/SuperCollider/Extensions/
-endef
-NORNS_POST_INSTALL_TARGET_HOOKS += NORNS_POST_INSTALL_TARGET_USER
-
-# define NORNS_POST_INSTALL_TARGET_EXTENSIONS
-#   mkdir -p $(TARGET_DIR)/usr/share/SuperCollider/Extensions
-#   for file in $$(find $(@D)/sc -name "*.sc"); do \
-#     cp $${file} $(TARGET_DIR)/usr/share/SuperCollider/Extensions/; \
-#   done
-# endef
-# NORNS_POST_INSTALL_TARGET_HOOKS += NORNS_POST_INSTALL_TARGET_EXTENSIONS
-
 $(eval $(waf-package))
